c620b8b322a291177ca47d0c94977ea36214ecb0
Ben Sussman <benjamin.sussman@gmail.com>
2014-06-07 16:20:14 -0400

    directions are working

--- javascripts/search.js
+++ javascripts/search.js
@@ -1,6 +1,20 @@
+var directionsService = new google.maps.DirectionsService();
 
 var getDirections = function(){
-  var start = $(".start").val;
-  var end = $(".end").val;
+  var start = $(".start").val();
+  var end = $(".end").val();
+  
+  var request = {
+    origin:start,
+    destination:end,
+    travelMode: google.maps.TravelMode.DRIVING
+  };
+  directionsService.route(request, function(result, status) {
+    if (status == google.maps.DirectionsStatus.OK) {
+      directionsDisplay.setDirections(result);
+    }
+  });
+  
 };
+
 $("#findroute").click(getDirections);
