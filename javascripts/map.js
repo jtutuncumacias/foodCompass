4b8340915a06c60c3474390831dee4f82e5249b7
Ben Sussman <benjamin.sussman@gmail.com>
2014-06-07 15:48:34 -0400

    killed the pan, sattelite, pegman

--- javascripts/map.js
+++ javascripts/map.js
@@ -3,7 +3,10 @@
     var myLatlng = new google.maps.LatLng(-25.363882,131.044922);
     var mapOptions = {
       zoom: 4,
-      center: myLatlng
+      center: myLatlng,
+      streetViewControl: false,
+      mapTypeControl: false,
+      panControl: false
     }
     var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
