dca66c66e548c9709f9a49b4f21f578340439e57
giordan212 <giordan212@hotmail.com>
2014-06-21 17:38:51 -0400

    Added DRAGGABLE Directions

--- javascripts/search.js
+++ javascripts/search.js
@@ -22,17 +22,7 @@ var getDirections = function(){
   directionsService.route(request, function(result, status) {
     if (status == google.maps.DirectionsStatus.OK) {
       directionsDisplay.setDirections(result);
-      deletemarkerList();
-      var resultLeg = result.routes[0].legs[0];
-      console.log(resultLeg);
-      console.log(resultLeg.start_location);
-      var t_lat = Math.max(resultLeg.start_location.k, resultLeg.end_location.k);
-      var b_lat = Math.min(resultLeg.start_location.k, resultLeg.end_location.k);
       
-      var t_lon = Math.max(resultLeg.start_location.A, resultLeg.end_location.A);
-      var b_lon = Math.min(resultLeg.start_location.A, resultLeg.end_location.A);
-      
-      var def = NH.data.getHealthyMarkets(t_lat, b_lat, t_lon, b_lon);
     }
     else{
       alert ("Something went wrong.  Please try again.");
@@ -53,7 +43,20 @@ var findCurrentLoc = function(){
         alert("Geolocation is not supported by this browser.");
     }
 };
-
+var updatedDirections = function(){
+  deletemarkerList();
+  var result = directionsDisplay.getDirections();
+  var resultLeg = result.routes[0].legs[0];
+  console.log(resultLeg);
+  console.log(resultLeg.start_location);
+  var t_lat = Math.max(resultLeg.start_location.k, resultLeg.end_location.k);
+  var b_lat = Math.min(resultLeg.start_location.k, resultLeg.end_location.k);
+  
+  var t_lon = Math.max(resultLeg.start_location.A, resultLeg.end_location.A);
+  var b_lon = Math.min(resultLeg.start_location.A, resultLeg.end_location.A);
+  
+  var def = NH.data.getHealthyMarkets(t_lat, b_lat, t_lon, b_lon);
+}
 
 $("#findroute").click(getDirections);
 $('#geolocate-button').click(findCurrentLoc);
